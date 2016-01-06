a5d39b9baf3cb26d715f55ec69ae59bdeb78c7ad
camwhite <camw85@mac.com>
2016-01-06 00:34:39 -0500

    lotsa stuff done itsa go

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,13 +1,20 @@
 'use strict';
 
 angular.module('asheSiteApp')
-.controller('MainController', ($scope, $http) => {
+.controller('MainController', ($scope, $http, $timeout) => {
   $scope.user = {};
+  $scope.sent = false;
+  $scope.callout = `Don't be a stranger.. I want to hear from you!`
 
   $scope.send = (form) => {
     if(form.$valid) {
       $http.post('api/emails', $scope.user).then(() => {
         $scope.user = {};
+        $scope.sent = true;
+
+        $timeout(() => {
+          $scope.callout = `Thanks for reaching out! Look for a reply very soon :)`
+        }, 1500);
       });
     }
   }
--- client/components/scroll-trigger/scroll-trigger.directive.js
+++ client/components/scroll-trigger/scroll-trigger.directive.js
@@ -6,7 +6,7 @@ angular.module('asheSiteApp')
       restrict: 'A',
       link: (scope, elem, attrs) => {
         $($window).on('scroll', () => {
-          if($($window).scrollTop() > $(elem).offset().top - $($window).height() && !$(elem).hasClass('active')) {
+          if($($window).scrollTop() > $(elem).offset().top - $($window).height() + $(elem).height() && !$(elem).hasClass('active')) {
             elem.addClass('active');
           }
         })
